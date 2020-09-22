1ed38863c3c63f1091fe23cb40a12976250c8fb8
Tofu Gang <tofugangsw@gmail.com>
2020-09-22 20:25:21 +0200

    Ok, I can move things around, make an iterator with a string representation of a class. But something's missing, isn't it?

--- main.py
+++ main.py
@@ -2,6 +2,59 @@ from sys import exit
 
 ################################################################################
 
+class FizzBuzz(object):
+
+    def __init__(self, i: int):
+        """
+
+        """
+
+        self._i = i
+
+################################################################################
+
+    def __str__(self):
+        """
+
+        :return:
+        """
+
+        if self._i % 3 == 0 and self._i % 5 == 0:
+            return "fizz buzz"
+        elif self._i % 3 == 0:
+            return "fizz"
+        elif self._i % 5 == 0:
+            return "buzz"
+        else:
+            return str(self._i)
+
+################################################################################
+
+def ya_basic() -> str:
+    """
+    :return: The most basic fizz buzz I can think of. The string is made with
+    the same format as iterator output.
+    """
+
+    fizzbuzz = "["
+
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            fizzbuzz += "'fizz buzz'"
+        elif i % 3 == 0:
+            fizzbuzz += "'fizz'"
+        elif i % 5 == 0:
+            fizzbuzz += "'buzz'"
+        else:
+            fizzbuzz += "'" + str(i) + "'"
+        fizzbuzz += ", "
+    fizzbuzz = fizzbuzz[:-2]
+    fizzbuzz += "]"
+
+    return fizzbuzz
+
+################################################################################
+
 if __name__ == "__main__":
     """
     Players generally sit in a circle. The player designated to go first says 
@@ -15,15 +68,9 @@ if __name__ == "__main__":
     print("FizzBuzz")
     print("--------")
 
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("fizz buzz")
-        elif i % 3 == 0:
-            print("fizz")
-        elif i % 5 == 0:
-            print("buzz")
-        else:
-            print(i)
+    print(ya_basic())
+    # I think I can do an iterator:
+    print([str(FizzBuzz(i)) for i in range(1, 101)])
 
     exit(0)
 
